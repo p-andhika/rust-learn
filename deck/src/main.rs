429eba0d5caf923bba08678d9aadd36d0e4b5bdf
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-09-22 11:32:19 +0700

    fix(deck): make `cards` mutable to fix borrow error

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -1,1 +1,20 @@
-fn main() {}
+#[derive(Debug)]
+struct Deck {
+    cards: Vec<String>,
+}
+
+fn main() {
+    let suits = ["Diamond", "Spade"];
+    let values = ["Ace", "Two", "Three"];
+    let cards = vec![];
+
+    for suit in suits {
+        for value in values {
+            let card = format!("{} of {}", value, suit);
+            cards.push(card);
+        }
+    }
+
+    let deck = Deck { cards: vec![] };
+    println!("Here's your deck, {:?}", deck);
+}
