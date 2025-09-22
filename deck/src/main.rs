c197f60820d5a15d9cb4a84da4fcbdc168a8814a
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-09-22 13:42:45 +0700

    refactor(deck): move deck creation to `Deck::new`

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -3,18 +3,27 @@ struct Deck {
     cards: Vec<String>,
 }
 
-fn main() {
-    let suits = ["Diamond", "Spade"];
-    let values = ["Ace", "Two", "Three"];
-    let mut cards = vec![];
+impl Deck {
+    fn new() -> Self {
+        let suits = ["Diamond", "Spade"];
+        let values = ["Ace", "Two", "Three"];
+        let mut cards = vec![];
 
-    for suit in suits {
-        for value in values {
-            let card = format!("{} of {}", value, suit);
-            cards.push(card);
+        for suit in suits {
+            for value in values {
+                let card = format!("{} of {}", value, suit);
+                cards.push(card);
+            }
         }
-    }
 
-    let deck = Deck { cards };
-    println!("Here's your deck, {:#?}", deck);
+        let deck = Deck { cards };
+
+        return deck;
+    }
+}
+
+fn main() {
+    let deck = Deck::new();
+
+    println!("Here is your deck: {:#?}", deck);
 }
