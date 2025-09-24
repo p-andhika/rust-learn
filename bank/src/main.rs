5a5bdf0bb8f4288a7a42bc36c77c4ee6933f441d
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-09-24 13:46:19 +0700

    refactor: extract account printing into a function

--- bank/src/main.rs
+++ bank/src/main.rs
@@ -26,10 +26,14 @@ impl Bank {
     }
 }
 
+fn print_account(account: Account) {
+    println!("{:#?}", account);
+}
+
 fn main() {
     let bank = Bank::new();
     let account = Account::new(1, String::from("Prakas"));
 
     println!("{:#?}", bank);
-    println!("{:#?}", account);
+    print_account(account);
 }
