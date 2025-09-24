b9a3aa009c5710db33eb13dde5663db4979508d7
Andhika Prakasiwi <p.andhika@op.iitg.ac.in>
2025-09-24 11:33:38 +0700

    feat: initialize bank project with basic structs

---
+++ bank/target/debug/deps/bank-20e3e9b1d01c9926.d
@@ -0,0 +1,9 @@
+/Users/andhikaprakasiwi/Documents/learn/rust-learn/bank/target/debug/deps/bank-20e3e9b1d01c9926.d: src/main.rs Cargo.toml
+
+/Users/andhikaprakasiwi/Documents/learn/rust-learn/bank/target/debug/deps/libbank-20e3e9b1d01c9926.rmeta: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
---
+++ bank/target/debug/deps/bank-e81b28b53823c2a4.d
@@ -0,0 +1,9 @@
+/Users/andhikaprakasiwi/Documents/learn/rust-learn/bank/target/debug/deps/bank-e81b28b53823c2a4.d: src/main.rs Cargo.toml
+
+/Users/andhikaprakasiwi/Documents/learn/rust-learn/bank/target/debug/deps/libbank-e81b28b53823c2a4.rmeta: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=--no-deps__CLIPPY_HACKERY__
+# env-dep:CLIPPY_CONF_DIR
